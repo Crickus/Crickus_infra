8d4b4d19b82b48d165339128b1bf60b88e4d1146
Dimka <22008248+Crickus@users.noreply.github.com>
2022-01-10 20:17:44 +0200

    New modules

---
+++ terraform/modules/app/outputs.tf
@@ -0,0 +1,7 @@
+output "app_external_ip2" {
+  value = "${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
+}
+
+# output "Global Forwarding Rule IP" {
+#   value = "${google_compute_forwarding_rule.lb_firewall.ip_address}"
+# }
