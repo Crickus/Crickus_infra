5591a54e3ec2bdea605c118c7ce8445b886edf86
Dimka <22008248+Crickus@users.noreply.github.com>
2022-01-10 15:19:52 +0200

    diversification

--- terraform/app.tf
+++ terraform/app.tf
@@ -38,7 +38,7 @@ resource "google_compute_instance" "app" {
 #   }
 }
 
-resource "google_compute_address" "ap_ip" { name = "reddit-app-ip" }
+resource "google_compute_address" "app_ip" { name = "reddit-app-ip" }
 
 resource "google_compute_firewall" "firewall_puma" {
   name    = "allow-puma-default"
