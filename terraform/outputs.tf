01d6b78dc88f6ffe1535536db662be7d97c2f974
Dimka <22008248+Crickus@users.noreply.github.com>
2022-01-10 13:49:47 +0200

    Tf changes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "app_external_ip" {
   value = "${google_compute_instance.app.*.network_interface.0.access_config.0.nat_ip}"
 }
 
-output "Global Forwarding Rule IP" {
-  value = "${google_compute_forwarding_rule.lb_firewall.ip_address}"
-}
+# output "Global Forwarding Rule IP" {
+#   value = "${google_compute_forwarding_rule.lb_firewall.ip_address}"
+# }
