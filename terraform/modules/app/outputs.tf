40c90534144caf6a30615f0c11226494d3a18517
Dimka <22008248+Crickus@users.noreply.github.com>
2022-01-23 12:57:25 +0200

    Playbooks update

--- terraform/modules/app/outputs.tf
+++ terraform/modules/app/outputs.tf
@@ -1,4 +1,4 @@
-output "app_external_ip2" {
+output "app_pip" {
   value = "${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
 }
 
--- terraform/stage/modules/app/files/set_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-DATABASE_URL="${1:-127.0.0.1:27017}"
-
-bash -c "echo 'DATABASE_URL=${DATABASE_URL}' > ~/reddit_app_service.conf"
